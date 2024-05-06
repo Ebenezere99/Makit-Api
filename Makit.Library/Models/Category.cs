bb9b95a265327dbc810b067f81241f87c2c98aa0
philippe ebenezere nga <philippeebenezere8@outlook.fr>
2024-05-06 02:17:52 +0100

    Add files via upload

---
+++ Makit.Library/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makit.Library.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        [MaxLength(3)]
+        [MinLength(3)]
+        [RegularExpression(@"^[A-Z][0-9][0-9]*$")]
+        public string? Reference { get; set; }
+
+        [MinLength(7)]
+        [MaxLength(37)]
+        [Required]
+        public string? Name { get; set; }
+    }
+}
---
+++ Makit.Library/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makit.Library.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        [MaxLength(7)]
+        [MinLength(7)]
+        [RegularExpression(@"^[A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z]*$")]
+        public string? Reference { get; set; }
+
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(37)]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public string? Image { get; set; }
+        
+        [Required]
+        [MaxLength(3)]
+        [MinLength(3)]
+        [RegularExpression(@"^[A-Z][0-9][0-9]*$")]
+        public string? CategoryReference { get; set; }
+
+        [Required]
+        public ulong Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+       
+        
+    }
+}
